df7a338c6ebae6025f7134aa644d3b4f033cc809
Muwavictoria <victoriamuwanguzi19@gmail.com>
2022-09-08 11:25:32 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-/*
+#include <studio.h>
+
+/**
+*main - primt the string in the put function
+**Description: using the main function
+*this program prints "with proper grammar, but the outcome is a piece of art"
+*
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is 
+
+a piece of art", \n"); 
+	return(0);
+}
