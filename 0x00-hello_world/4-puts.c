5ecbc1992434b6bcffff3a501804d35faa261ced
Muwavictoria <victoriamuwanguzi19@gmail.com>
2022-09-08 11:06:01 -0700

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+* main -print th put function
+*
+*Description: using the main function
+*This program prints the "Programming is like building a multilingual puzzle, followed by a new line.
+*returns 0
+*/
+int main(void)
+{
+	puts("\" Programming is like building a uzzle");
+	return(0);
+}
